58bd02cc6ec43a5c47191d79e89d59c14e650c03
fiftycommit <fiftycommit@users.noreply.github.com>
2026-01-16 11:22:07 +0000

    deploy: 273afe2637d621e0f43afd866b078f867b00971e

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -87,9 +87,9 @@ const RESOURCES = {"flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "canvaskit/canvaskit.js.symbols": "a3c9f77715b642d0437d9c275caba91e",
 "canvaskit/skwasm.js.symbols": "3a4aadf4e8141f284bd524976b1d6bdc",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"flutter_bootstrap.js": "5d2aaf992597d242cfb68b1e457c506b",
+"flutter_bootstrap.js": "b1b30115a5201aa8c4b9fc30a87cf602",
 "version.json": "7bf9a2beddd340869244059b9a3d8466",
-"main.dart.js": "2af14ff26d889a8b005ba69a54c1a3ce"};
+"main.dart.js": "9ea30b030369e91eecda89c265a50269"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
