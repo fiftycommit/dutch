6a21a7b6252637322e3e0c93ac417b415104af5b
Max M'BEY <imaxmbey@gmail.com>
2026-02-25 21:10:20 +0100

    rechargement normal tentative 3

--- lib/services/web/web_session_storage.dart
+++ lib/services/web/web_session_storage.dart
@@ -1,35 +1,29 @@
-import 'dart:js_interop';
-import 'dart:js_interop_unsafe';
+import 'package:web/web.dart' as web;
 
 /// Persists room session data in sessionStorage for web reload recovery.
 class WebSessionStorage {
   static const _roomKey = 'dutch_room_code';
   static const _routeKey = 'dutch_route';
 
-  static JSObject get _storage =>
-      globalContext.getProperty('sessionStorage'.toJS) as JSObject;
-
   static void saveSession(String roomCode) {
     try {
-      _storage.callMethod('setItem'.toJS, _roomKey.toJS, roomCode.toJS);
+      web.window.sessionStorage.setItem(_roomKey, roomCode);
     } catch (_) {}
   }
 
   static void saveRoute(String? route) {
     try {
       if (route != null) {
-        _storage.callMethod('setItem'.toJS, _routeKey.toJS, route.toJS);
+        web.window.sessionStorage.setItem(_routeKey, route);
       } else {
-        _storage.callMethod('removeItem'.toJS, _routeKey.toJS);
+        web.window.sessionStorage.removeItem(_routeKey);
       }
     } catch (_) {}
   }
 
   static String? getRoomCode() {
     try {
-      final val =
-          _storage.callMethod('getItem'.toJS, _roomKey.toJS) as JSString?;
-      return val?.toDart;
+      return web.window.sessionStorage.getItem(_roomKey);
     } catch (_) {
       return null;
     }
@@ -37,9 +31,7 @@ class WebSessionStorage {
 
   static String? getRoute() {
     try {
-      final val =
-          _storage.callMethod('getItem'.toJS, _routeKey.toJS) as JSString?;
-      return val?.toDart;
+      return web.window.sessionStorage.getItem(_routeKey);
     } catch (_) {
       return null;
     }
@@ -47,8 +39,8 @@ class WebSessionStorage {
 
   static void clearSession() {
     try {
-      _storage.callMethod('removeItem'.toJS, _roomKey.toJS);
-      _storage.callMethod('removeItem'.toJS, _routeKey.toJS);
+      web.window.sessionStorage.removeItem(_roomKey);
+      web.window.sessionStorage.removeItem(_routeKey);
     } catch (_) {}
   }
 }
