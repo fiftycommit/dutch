b67df1bc4ac7dc921d74e484e0531de7ae347a9d
Max MBEY <imaxmbey@gmail.com>
2026-06-26 02:21:35 +0000

    chore: retirer dist/ et node_modules/ de l'index git
    dutch-server/dist/ et dutch-server/node_modules/ étaient déjà dans
    .gitignore mais avaient été committés avant l'ajout de ces règles.
    Retrait de l'index via git rm --cached (fichiers conservés sur disque).
    Ajout de /package-lock.json à .gitignore (artefact npm généré à la
    racine par erreur, le projet Node est dans dutch-server/).

--- dutch-server/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-void (0, server_1.startServer)().catch((error) => {
-    console.error('Fatal server startup error:', error);
-    process.exit(1);
-});
-//# sourceMappingURL=index.js.map
